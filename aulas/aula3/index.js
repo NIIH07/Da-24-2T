ebe75db73b8b2434d477888cf35a48941ffd39fa
JoaoH123 <joaoh220708@gmail.com>
2024-08-15 14:18:48 -0300

    primeiro trabalho avaliativo

---
+++ aulas/aula3/index.js
@@ -0,0 +1,59 @@
+// texto exemplo
+
+/* texto comentado
+linha 1
+linha 2 
+linha 3 */
+
+console.log("olá,mundo!")
+
+console.log ("turma 24-2T")
+
+console.log("o que é a vida ?")
+
+prompt("qual o seu nome ?")
+const nome = "ingrid"
+const idade = 16 
+console.log ("olá", "meu nome é:" , nome , "e eu tenho:" , idade, "anos");
+
+const sobrenome = "nascimento"
+const estudante = "estudo"
+
+console.log ("olá", "meu nome é:" , nome ,  sobrenome , "e eu tenho:" , idade , "anos" , "e eu" , estudante);
+
+const got = "stranger things "
+const temporadasDeGot = "4";
+
+console.log (typeof got)
+console.log (typeof temporadasDeGot)
+
+let novaVariavel
+console.log (typeof novaVariavel)
+
+novaVariavel = 2 
+console.log ( typeof novaVariavel)
+
+novaVariavel = undefined
+console.log(novaVariavel)
+
+//const novoNome = prompt ( "seu nome mana ");
+//console.log (novoNome )
+
+//const novidade = prompt ( "qual a tua idade ?");
+//console.log (novidade)
+
+console.log(typeof novoNome)
+console.log(typeof novidade)
+
+const idadeNumero = 76
+const idadeTexto = idadeNumero.toString()
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto )
+
+const idadeTexto2 = "23"
+const idadeNumero2 = number(idadeTexto2)
+console.log(typeof idadeTexto2)
+console.log(typeof idadeNumero2)
+
+
+
---
+++ trabalho avaliativo/index.js
@@ -0,0 +1,38 @@
+//trabalho avaliativo, vamos converter graus celsius para fahrenheit 
+//vamos fazer com console.log e prompt 
+/* 
+
+     celsius               fahrenheit               o que o programa irá responder 
+     38                      100.4                              100,4
+     23                       73;4                               73,4
+     12                       53,6                               53,4
+     7                        44,6                               44,6
+     -28                     -18,4                              -18
+     -13                       8,6                                8,6
+
+     numero de tentativas :
+     1. na primeira tentamos fazer o de impar ou par mas acabou que achamos mais facil fazer em celsius
+     2. acabamos trocando cons por let e tivemos que trocar de novo.
+     3. acabamos esquecendo de por as aspas na palavras, e não estavamos conseguindo achar, ficamos um tempinho procurando.
+     4. acabamos esquecendo de por o + 32 para dar o resultado, acabando não respondendo nossas questões.
+     
+     solução 
+      
+     acabamos indo no google indo no arquivo drive e tambem pesquisando no youtube, e acabamos encontrando respostas 
+
+     nosso algoritimo falhou 3 vezes, mas na ultima tentativa funcionou 
+     */
+    
+     let celsius
+     let fahrenheit
+     
+     celsius = prompt("qual e o grau celsius?")
+     console.log("o numero fica em celsius ",celsius)
+     fahrenheit = (celsius *1.8) + 32
+     console.log("em fahrenheit ficara", fahrenheit)
+     
+
+     
+     
+
+  
