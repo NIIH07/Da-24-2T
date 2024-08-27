d725fcd24f2707c4f6aaba71618413c7e207940a
JoaoH123 <joaoh220708@gmail.com>
2024-08-27 17:23:29 -0300

    aula 10

---
+++ aulas/aula10/index.js
@@ -0,0 +1,192 @@
+/*
+let condicao1 = true
+
+if(condicao1){
+    //acao
+    console.log("exibiu condicao1")
+}
+
+let condicao2 = false 
+
+if(condicao2){
+    //acao
+    console.log("exibiu condicao2")
+}
+    */
+
+/*
+const num1 = (prompt ("digite o primeiro numero "))
+const num2 = (prompt ("digite o segundo numero "))
+
+if(num1 === num2){
+    console.log("os numeros sao iguais?")
+}
+
+if(num1==num2){
+    console.log("os numeros sao diferentes")
+}
+*/
+
+/*
+if(condicao){
+    console.logu("a condicao é verdadeira")
+}else{
+    console.log("a condicao é falsa")
+}
+*/
+
+/*
+const num1 = (prompt ("digite o primeiro numero "))
+const num2 = (prompt ("digite o segundo numero "))
+
+if(num1 === num2){
+    console.log("os numeros sao iguais?")
+}else{
+
+if(num1==num2)
+    console.log("os numeros sao diferentes")
+}
+*/
+ /*
+let condicao1 = false 
+let condicao2 = false
+
+if(condicao1){
+    console.log("condicao1 é verdadeira")
+}else if(condicao2){
+    if(condicao2){
+        console.log("condicao2 é verdadeira")
+    }
+}else{
+    console.log("entrou no else ")
+}
+ */
+
+/*
+const num1 = (prompt ("digite o primeiro numero "))
+
+const num2 = (prompt ("digite o segundo numero "))
+
+if(num1===num2){
+    console.log("os numeros sao igauis")
+}else if (num1>num2){
+    console.log("o numero1 é maior que o numero2")
+}
+    */
+
+   /*
+
+let paisDeOrigem = prompt("digite o pais de origem")
+
+switch(paisDeOrigem){
+    case"Brasil":
+    console.log("Brasileiro")
+    break 
+    case"EUA":
+    console.log("Norte Americano")
+    break 
+    case"Inglaterra":
+    console.log("Inglês")
+    break
+    default :
+    console.log("nenhuma nacionalidade encontrada")
+    break
+}
+*/
+/*
+let Pokémons = prompt("digite um pokémon")
+switch(Pokémons){
+    case"Bulbassauro":
+    console.log("o Pokémon escolhido foi o Bulbassauro do tipo Planta e Veneno")
+    break 
+    case"Charmander":
+    console.log("o Pokémon escolhido foi o Charmander do tipo Fogo")
+    break 
+    case"Squirtle":
+    console.log(" o Pokémon escolhido foi o Squirtle do tipo Àgua")
+    break 
+    default :
+    console.log("nenhum Pokémon encontrado")
+    break
+
+}
+    */
+    
+/*
+const chapéuCeletor = prompt("digite seu nome para saber a sua casa em Hogwarts")
+switch(chapéuCeletor){
+    case"rony weasley":
+    console.log("a sua casa é Grifinória" )
+    break
+    case"ana":
+    console.log("a sua casa é Lufa-Lufa ")
+    break
+    case"luna":
+    console.log("a sua casa é Corvinal")
+    break 
+    case"narcisa":
+    console.log("a sua casa é Sonserina")
+    break 
+    case"fredy":
+    console.log("nenhuma casa encontrada")
+    break
+
+}
+    */
+
+/*
+let condicao1 = true 
+let condicao2 = false
+
+if (condicao1 && condicao2){
+    console.log("As duas condicoes são verdadeiras ")
+}else{
+    console.log("Alguma condicao é falsa")
+}
+
+if (condicao1 || condicao2){
+    console.log("Alguma das condicoes é verdadeira ")
+}else{
+    console.log("Todas são falsas")
+}
+
+if (!condicao1){
+    console.log("A condicao1 é verdadeira ")
+}else{
+    console.log("A condicao1 é falsa")
+}
+    */
+   /*
+
+let ensinoMedio=prompt("você já completou o ensino medio?")
+let idade=prompt("você tem 18 anos ou mais ?")
+let faculdade=prompt("você está cursando em alguma outra facultade?")
+
+switch(idade){
+    case "18 >":
+    console.log("voce pode fazer a faculdade")
+    break
+     case"18<":
+    console.log("voce nao pode fazer facultade")
+    break
+    
+}
+
+switch(ensinoMedio){
+    case "sim":
+    console.log("voce pode fazer a faculdade")
+    break
+    case "nao":
+    console.log("voce nao pode fazer a faculdade")
+    break
+}
+
+switch(faculdade){
+    case "nao":
+        console.log("voce pode fazer a faculdade")
+        break
+        case "sim":
+            console.log("voce nao pode fazer a faculdade")
+            break
+}
+*/
